5c8ad49ba832915b0d8795e671e23b87da5d9451
armaBurton <armanarma@armaburton.com>
2022-01-26 16:50:51 -0800

    stoplight function is working, forgot to push after finishing it, vehiclelist is working

--- src/VehicleList.js
+++ src/VehicleList.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Vehicle from './Vehicle';
 
-export default function VehicleList() {
+export default function VehicleList(props) {
+  console.log(props.vehicleArr);
   return (
     <div className="traffic">
+      {props.vehicleArr.map((vehicle, i) => <Vehicle key={vehicle + i} vehicle={vehicle} />)}
+      {/* {props.animalArray.map((animal, i) => <AnimalEmoji key={animal + i} animal={animal} />)} */}
       {/* this component takes in a prop called vehicles, which is an array of vehicle strings */}
       {/* map through the vehicles array in props */}
-      {/* for each item render a Vehicle component. Pass the vehicl string as a prop called vehicle to the Vehicle component.  */}
+      {/* for each item render a Vehicle component. Pass the vehicle string as a prop called vehicle to the Vehicle component.  */}
     </div>
   );
 }
